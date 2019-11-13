d707a01c76a36bc0cd642d54529453c51b7860eb
liufw <liufw@chinamobiad.com>
2019-11-13 16:32:53 +0800

    first commit

---
+++ android/app/src/main/java/com/reactnativeumengstarter/MainActivity.java
@@ -0,0 +1,29 @@
+package com.reactnativeumengstarter;
+
+import com.facebook.react.ReactActivity;
+import com.umeng.analytics.MobclickAgent;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ReactNativeUmengStarter";
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    MobclickAgent.onResume(this);
+
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    MobclickAgent.onPause(this);
+  }
+}
---
+++ android/app/src/main/java/com/reactnativeumengstarter/MainApplication.java
@@ -0,0 +1,79 @@
+package com.reactnativeumengstarter;
+
+import android.app.Application;
+import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import com.example.umenganaticlys.UmengAnalyticsPackage;
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.commonsdk.UMConfigure;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+    UMConfigure.init(this, "5d7488cb0cafb200e6000b7d", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, null);
+    MobclickAgent.openActivityDurationTrack(false);
+  }
+
+  /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+        We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
